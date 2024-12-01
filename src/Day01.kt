53a2ad752072c1883987de22db64be4aa6f86849
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-01 13:29:51 +0800

    Extract `processInput`

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,14 +1,17 @@
 import kotlin.math.abs
 
 fun main() {
+    fun processInput(input: List<String>) =
+        input.map { it.split(Regex(" +")).map { it.toInt() } }
+
     fun part1(input: List<String>): Int {
-        val inputPairs = input.map { it.split(Regex(" +")).map { it.toInt() } }
+        val inputPairs = processInput(input)
         val paired = inputPairs.map { it[0] }.sorted() zip inputPairs.map { it[1] }.sorted()
         return paired.sumOf { abs(it.first - it.second) } // The bound should be 100000 * 2 * 1000 so `Int` should be fine.
     }
 
     fun part2(input: List<String>): Long {
-        val inputPairs = input.map { it.split(Regex(" +")).map { it.toInt() } }
+        val inputPairs = processInput(input)
         val leftSet = inputPairs.asSequence().map { it[0] }.groupingBy { it }.eachCount()
         val right = inputPairs.asSequence().map { it[1] }//.also { println(it.toList()) }
         return right.map { it.toLong() * (leftSet[it] ?: 0).toLong() }/*.also { println(it.toList()) }*/.sum() // bigger bound
