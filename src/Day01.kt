be4bb54b0ee905f8ecc8c147f7578ab59a497ec8
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-01 13:28:48 +0800

    Complete Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,30 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputPairs = input.map { it.split(Regex(" +")).map { it.toInt() } }
+        val paired = inputPairs.map { it[0] }.sorted() zip inputPairs.map { it[1] }.sorted()
+        return paired.sumOf { abs(it.first - it.second) } // The bound should be 100000 * 2 * 1000 so `Int` should be fine.
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val inputPairs = input.map { it.split(Regex(" +")).map { it.toInt() } }
+        val leftSet = inputPairs.asSequence().map { it[0] }.groupingBy { it }.eachCount()
+        val right = inputPairs.asSequence().map { it[1] }//.also { println(it.toList()) }
+        return right.map { it.toLong() * (leftSet[it] ?: 0).toLong() }/*.also { println(it.toList()) }*/.sum() // bigger bound
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("1  2")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+
+    check(part2(testInput)/*.also { println(it) }*/ == 31L)
     part2(input).println()
 }
