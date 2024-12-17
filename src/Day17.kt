76802efe2105072e919970c1cf8ae9f34efec82b
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-17 17:43:00 +0800

    Complete the optimized search but get a wrong answer

--- src/Day17.kt
+++ src/Day17.kt
@@ -126,8 +126,8 @@ fun main() {
     }
 
     fun part2Search(input: List<String>): Int {
-        var time = System.currentTimeMillis()
         val (registers, program) = processInput(input)
+        var time = System.currentTimeMillis()
         // To see the debug output, don't use a parallel stream.
         val ans = (1..Int.MAX_VALUE).asSequence().asStream().parallel().filter { registerA ->
             if (registerA % (1 shl 24) == 0) {
@@ -147,9 +147,9 @@ fun main() {
                 programIterator.hasNext() &&
                         // `equals` not available for a value class
                         it.uByte == programIterator.next().uByte
-                        /*(it.uByte.also { println("Output: $it") } == programIterator.next().uByte.also { println("Program element: $it") }).also {
-                            println("Comp result: $it")
-                        }*/
+                /*(it.uByte.also { println("Output: $it") } == programIterator.next().uByte.also { println("Program element: $it") }).also {
+                    println("Comp result: $it")
+                }*/
             } &&
                     !programIterator.hasNext()// make sure that all the elements are compared
         }
@@ -159,7 +159,7 @@ fun main() {
         return ans
     }
 
-    fun part2Analyze() {
+    fun part2SearchWithMyPuzzleInputWithAnalysis(): Long {
         /*
         Program: 2,4,1,4,7,5,4,1,1,4,5,5,0,3,3,0
 
@@ -169,7 +169,7 @@ fun main() {
         Instruction 2 (7,5): a, (a mod 8) xor 4, a / (2 ^ ((a mod 8) xor 4))
         Instruction 3 (4,1): a, ((a mod 8) xor 4) xor (a / (2 ^ ((a mod 8) xor 4))), a / (2 ^ ((a mod 8) xor 4))
         Instruction 4 (1,4): a, ((a mod 8) xor 4) xor (a / (2 ^ ((a mod 8) xor 4))) xor 4 = (a mod 8) xor (a / (2 ^ ((a mod 8) xor 4))), a / (2 ^ ((a mod 8) xor 4))
-        Instruction 5 (5,5): output "(a mod 8) xor (a / (2 ^ ((a mod 8) xor 4))) mod 8"
+        Instruction 5 (5,5): output "(a mod 8) xor (a / (2 ^ ((a mod 8) xor 4))) mod 8 = (a mod 8) xor (a shr ((a mod 8) xor 4) mod 8)"
         Instruction 6 (0,3): a / (2 ^ 3), (a mod 8) xor (a / (2 ^ ((a mod 8) xor 4))), a / (2 ^ ((a mod 8) xor 4))
 
           for a = 511: 63, 7 xor 63 = 56, a / (2 ^ (7 xor 4 = 3))) = 63 (manually verified correct)
@@ -183,6 +183,54 @@ fun main() {
 
         Discovery: for the output, only `a`'s low 10 bits are involved as `(a mod 8) xor 4)` maximizes at 7, and only 3 bits out of them are involved, depending on `a mod 8`.
          */
+
+
+        val bitFullSpace = listOf(false, true)
+        val intialSpace = List<Boolean?>(48) { null }
+        val program = listOf(2, 4, 1, 4, 7, 5, 4, 1, 1, 4, 5, 5, 0, 3, 3, 0)
+
+        fun search(groupIndex: Int, space: List<Boolean?>): Long? =
+            if (groupIndex >= 16) {
+                //if (space.all { it !== null })
+                space.withIndex().sumOf { (i, b) -> (if (b!!) 0L else 1L) shl i }
+                //else null
+            } else {
+                // not used
+                fun bitSpace(index: Int) =
+                    space[index]?.let { listOf(it) } ?: bitFullSpace
+
+                (0 until 8).firstNotNullOfOrNull { am8 ->
+                    println("groupIndex=$groupIndex, am8=$am8")
+
+                    val mutableSpace = space.toMutableList()
+                    fun checkAndSetSpace(m8: Int, startIndex: Int): Boolean {
+                        val bits = (0 until 3).map { i -> m8 shr i and 1 == 1 }
+                        bits.forEachIndexed { i, b ->
+                            val index = startIndex + i
+                            if (index < 48) {
+                                val existing = mutableSpace[index]
+                                if (existing === null)
+                                    mutableSpace[index] = b
+                                else if (existing != b)
+                                    return false
+                            }
+                        }
+                        return true
+                    }
+
+                    val startIndex = groupIndex * 3
+                    if (!checkAndSetSpace(am8, startIndex)) return@firstNotNullOfOrNull null
+
+                    val involvedUpper = program[groupIndex] xor am8
+                    println("programElement=${program[groupIndex]}, involvedUpper=$involvedUpper")
+                    val involvedUpperStartIndex = startIndex + (am8 xor 4)
+                    if (!checkAndSetSpace(involvedUpper, involvedUpperStartIndex)) return@firstNotNullOfOrNull null
+
+                    search(groupIndex + 1, mutableSpace)
+                }
+            }
+
+        return search(0, intialSpace)!!
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -200,5 +248,6 @@ fun main() {
     //check(part2Search(testInput2).also { println(it) } == 117440)
     println("Check passed.")
     // On my computer it takes 0.5s to search 2 ^ 24 runs, so for 2 ^ 48 runs it's impossible to search.
-    part2Search(input).println()
+    //part2Search(input).println()
+    part2SearchWithMyPuzzleInputWithAnalysis().println()
 }
