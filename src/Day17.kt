19632af3dde9eff241793f06c2890776d41a1632
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-17 13:43:37 +0800

    Complete Part 1 of Day 17

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,6 +1,87 @@
+import kotlin.math.pow
+
+@JvmInline
+value class `3BitInteger`(val uByte: UByte) {
+    init {
+        require(uByte in 0U until 8U)
+    }
+
+    override fun toString() =
+        uByte.toString()
+}
+
+sealed class ComboOperand {
+    class Value(val value: `3BitInteger`) : ComboOperand()
+    class Register(val index: Int) : ComboOperand()
+}
+
+fun String.to3BitInteger() =
+    `3BitInteger`(toUByte())
+
+fun Int.pow(n: Int) =
+    toDouble().pow(n).toInt()
+
+fun `3BitInteger`.toInt() =
+    uByte.toInt()
+
+fun Int.to3BitInteger() =
+    `3BitInteger`(toUByte())
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): String {
+        //val emptyLineIndex = input.indexOf("")
+        val registers = input.asSequence().take(3).map { it.substringAfter(": ").toInt() }.toList()
+            .toIntArray() // TODO `BigInteger` instead?
+        val program = input[4].substringAfter(": ").splitToSequence(',').map { it.to3BitInteger() }.toList()
+
+        var outputs = mutableListOf<`3BitInteger`>()
+
+        var ip = 0
+        //for ((opcode, literalOperand) in program.chunked(2)) {
+        while (ip < program.size) {
+            val opcode = program[ip]
+            val literalOperand = program[ip + 1]
+
+            fun comboOperand() =
+                when (val uByte = literalOperand.uByte) {
+                    0.toUByte(), 1.toUByte(), 2.toUByte(), 3.toUByte() -> ComboOperand.Value(literalOperand)
+                    4.toUByte(), 5.toUByte(), 6.toUByte() -> ComboOperand.Register(uByte.toInt() - 4)
+                    7.toUByte() -> throw IllegalArgumentException()
+                    else -> throw AssertionError()
+                }
+
+            fun comboOperandValue() =
+                when (val comboOperand = comboOperand()) {
+                    is ComboOperand.Value -> comboOperand.value.toInt()
+                    is ComboOperand.Register -> registers[comboOperand.index]
+                }
+
+            var incIP = true
+            fun dv() =
+                registers[0] / 2.pow(comboOperandValue())
+            when (opcode.uByte) {
+                0.toUByte() -> registers[0] = dv()
+                1.toUByte() -> registers[1] = registers[1] xor literalOperand.toInt()
+                2.toUByte() -> registers[1] = comboOperandValue() % 8
+                3.toUByte() -> {
+                    val aRegister = registers[0]
+                    if (aRegister != 0) {
+                        ip = literalOperand.toInt()
+                        incIP = false
+                    }
+                }
+
+                4.toUByte() -> registers[1] = registers[1] xor registers[2]
+                5.toUByte() -> outputs.add((comboOperandValue() % 8).to3BitInteger())
+                6.toUByte() -> registers[1] = dv()
+                7.toUByte() -> registers[2] = dv()
+                else -> throw IllegalArgumentException()
+            }
+            if (incIP)
+                ip += 2
+        }
+
+        return outputs.joinToString(",")
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +89,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day17_test.txt` file:
     val testInput = readInput("Day17_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput)/*.also { println(it) }*/ == "4,6,3,5,6,3,5,2,1,0")
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day17")
