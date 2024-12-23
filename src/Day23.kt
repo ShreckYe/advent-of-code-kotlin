6eed94dc34c0be4b3173858a6570b8f8d25894dd
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-23 13:13:54 +0800

    Add an incorrect solution for Part 1 of Day 23

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,6 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val connections = input.map {
+            with(it.split('-')) {
+                this[0] to this[1]
+            }
+        }
+
+        val connectionMap = (connections + connections.map { it.second to it.first })
+            .groupBy { it.first }
+            .mapValues { it.value.map { it.second } }
+
+        println(connectionMap)
+
+        val sets = connectionMap.asSequence()
+            .mapNotNull {
+                val connecteds = it.value
+                if (connecteds.size == 2 && (connectionMap[connecteds[0]]?.contains(connecteds[1]) ?: false))
+                    setOf(it.key) + connecteds
+                else
+                    null
+            }
+            .also { println(it.toList()) }
+            .distinct()
+            .also { println(it.toList()) }
+            .filter {
+                it.any { it[0] == 't' }
+            }
+            .also { println(it.toList()) }
+            .toSet()
+
+        return sets.size
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +37,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day23_test.txt` file:
     val testInput = readInput("Day23_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput).also { println(it) } == 7)
 
     // Read the input from the `src/Day23.txt` file.
     val input = readInput("Day23")
