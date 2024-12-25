b31be456f686ae55806b91f9c771b13ccb041fb3
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-25 13:36:00 +0800

    Complete Day 25

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,6 +1,32 @@
+enum class LockOrKey {
+    Lock, Key
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val emptyLineIndices = input.asSequence().withIndex().filter { it.value == "" }.map { it.index }
+        val lockOrKeyHeightss = (sequenceOf(-1) + emptyLineIndices + sequenceOf(input.size)).zipWithNext { a, b ->
+            val schematic = input.subList(a + 1, b)
+            if (schematic.first().all { it == '#' })
+                LockOrKey.Lock to (0 until 5).map { j ->
+                    schematic.subList(1, schematic.size).count { line -> line[j] == '#' }
+                }
+            else if (schematic.last().all { it == '#' })
+                LockOrKey.Key to (0 until 5).map { j ->
+                    schematic.subList(0, schematic.size - 1).count { line -> line[j] == '#' }
+                }
+            else
+                throw IllegalArgumentException()
+        }.toList()
+
+        val (locks, keys) = lockOrKeyHeightss.partition { it.first == LockOrKey.Lock }
+        val ans = locks.sumOf { lock ->
+            keys.count { key ->
+                (lock.second zip key.second).all { (lh, kh) -> lh + kh <= 5 }
+            }
+        }
+
+        return ans
     }
 
     fun part2(input: List<String>): Int {
@@ -8,16 +34,13 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day25_test.txt` file:
     val testInput = readInput("Day25_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
     // Read the input from the `src/Day25.txt` file.
     val input = readInput("Day25")
     part1(input).println()
-
-    check(part2(testInput) == 1) // TODO note that the test input might be different
-    part2(input).println()
 }
